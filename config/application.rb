bf4fadef4d350a05a0624cfaa91c118e9fa14a92
sbtr-sbtr <simke@ub.uni-leipzig.de>
2014-10-29 09:30:19 +0100

    skip validation of locale

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,9 @@ module Mdmap
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    # https://stackoverflow.com/questions/20361428/rails-i18n-validation-deprecation-warning
+    config.i18n.enforce_available_locales = false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Berlin'
